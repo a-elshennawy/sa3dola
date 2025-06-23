3820d847ba62d98b56b8e5c5dd1856773ccb7bbc
shennawy <ahmedshennawy997@gmail.com>
2025-06-23 19:52:37 +0300

    bar & footer

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,5 +1,15 @@
 import { useState, useEffect } from "react";
 import { PacmanLoader } from "react-spinners";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faWhatsapp,
+  faInstagram,
+  faTiktok,
+  faXTwitter,
+  faDiscord,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Header() {
   const [isLoading, setIsLoading] = useState(true);
@@ -36,11 +46,57 @@ export default function Header() {
           <div className="textContainer">
             <h2>أهلا بيك في منتجع الزعيم</h2>
             <h1>سعدولا</h1>
-            <h3>انزل تحت</h3>
-            <img
-              src="/images/icons8-arrow-down-60.png"
-              onContextMenu={(e) => e.preventDefault()}
-            />
+            <div className="socials row">
+              <div className="socialsHeader col-12">
+                <h1>احنا فين ؟</h1>
+                <a href="https://kick.com/sa3dola" target="_blank">
+                  <h1>kick</h1>
+                </a>
+              </div>
+              <div className="socialsContent col-12 row">
+                <span className="col-5 col-lg-1 youtube">
+                  <a href="https://www.youtube.com/@Sa3dola" target="_blank">
+                    <FontAwesomeIcon icon={faYoutube} />
+                  </a>
+                </span>
+                <span className="col-5 col-lg-1 facebook">
+                  <a href="https://www.facebook.com/Sa3dolaa/" target="_blank">
+                    <FontAwesomeIcon icon={faFacebook} />
+                  </a>
+                </span>
+                <span className="col-5 col-lg-1 whatsapp">
+                  <a
+                    href="https://www.whatsapp.com/channel/0029Vabs0vI4IBhL0ZMqwT07"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={faWhatsapp} />
+                  </a>
+                </span>
+                <span className="col-5 col-lg-1 instagram">
+                  <a href="https://www.instagram.com/sa3dolaa" target="_blank">
+                    <FontAwesomeIcon icon={faInstagram} />
+                  </a>
+                </span>
+                <span className="col-5 col-lg-1 tiktok">
+                  <a href="https://www.tiktok.com/@sa3dollaa" target="_blank">
+                    <FontAwesomeIcon icon={faTiktok} />
+                  </a>
+                </span>
+                <span className="col-5 col-lg-1 x">
+                  <a href="https://x.com/sa3dolaa" target="_blank">
+                    <FontAwesomeIcon icon={faXTwitter} />
+                  </a>
+                </span>
+                <span className="col-5 col-lg-1 discord">
+                  <a
+                    href="https://discord.com/invite/E6eZjb7YhW"
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon icon={faDiscord} />
+                  </a>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,17 +3,17 @@ import Header from "./Header/Header";
 import Events from "./Events/Events";
 import Youtube from "./Youtube/Youtube";
 import Moments from "./Moments/Moments";
-import Footer from "./Footer/Footer";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
 export default function Home() {
   return (
     <>
+      <ProgressBar />
       <Header />
       <Intro />
       <Youtube />
       <Events />
       <Moments />
-      <Footer />
     </>
   );
 }
