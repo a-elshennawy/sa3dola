7c2d6969a18d3ce31ee9cbe65f4be282fd1d4ca2
shennawy <ahmedshennawy997@gmail.com>
2025-07-06 21:26:49 +0300

    key

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,13 @@ import "./App.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY =
+  window.env?.VITE_CLERK_PUBLISHABLE_KEY ||
+  import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
